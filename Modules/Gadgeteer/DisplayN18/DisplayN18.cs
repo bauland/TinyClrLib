867d4d39fae03ed88bab1f9daf5b5127cb469619
Nicolas Bauland <nicolas.bauland@laposte.net>
2019-05-11 14:07:48 +0200

    [TIN-113] #done Bug fixed: add ChipSelectType in constructor

--- Modules/Gadgeteer/DisplayN18/DisplayN18.cs
+++ Modules/Gadgeteer/DisplayN18/DisplayN18.cs
@@ -71,7 +71,14 @@ namespace Bauland.Gadgeteer
             _backlightPin.SetDriveMode(GpioPinDriveMode.Output);
             _backlightPin.Write(GpioPinValue.Low);
 
-            _spi = SpiController.FromName(spiLine).GetDevice(new SpiConnectionSettings() {ChipSelectLine = chipSelectPin, ClockFrequency = 12000000, DataBitLength = 8, Mode = SpiMode.Mode0 });
+            _spi = SpiController.FromName(spiLine).GetDevice(new SpiConnectionSettings()
+            {
+                ChipSelectType = SpiChipSelectType.Gpio,
+                ChipSelectLine = chipSelectPin,
+                ClockFrequency = 12000000,
+                DataBitLength = 8,
+                Mode = SpiMode.Mode0
+            });
 
             Reset();
 
