da5e43776a3b3bf44653489617a0c1730abafd40
Nicolas Bauland <nicolas.bauland@laposte.net>
2019-06-25 20:52:01 +0200

    Begin implementation

---
+++ Modules/Others/MfRc522/Constant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace Bauland.Others
+{
+    namespace Constants.MfRc522
+    {
+        public static class Register
+        {
+            // Command and status
+            public static byte Command = 0x01 << 1;
+            public static byte TxASK = 0x15 << 1;
+            public static byte Mode = 0x11 << 1;
+        }
+    }
+}
--- Modules/Others/MfRc522/MfRc522.cs
+++ Modules/Others/MfRc522/MfRc522.cs
@@ -1,9 +1,83 @@
-﻿namespace Bauland.Others
+﻿using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Gpio;
+using GHIElectronics.TinyCLR.Devices.Spi;
+using Bauland.Others.Constants.MfRc522;
+
+namespace Bauland.Others
 {
     /// <summary>
     /// 
     /// </summary>
     public class MfRc522
     {
+        private GpioPin _resetPin;
+        private GpioPin _irqPin; // Can be null
+        private SpiDevice _spi;
+
+        public MfRc522(string spiBus, int resetPin, int csPin, int irqPin = -1)
+        {
+
+            var gpioCtl = GpioController.GetDefault();
+
+            _resetPin = gpioCtl.OpenPin(resetPin);
+            _resetPin.SetDriveMode(GpioPinDriveMode.Output);
+            _resetPin.Write(GpioPinValue.High);
+
+            if (irqPin != -1)
+            {
+                _irqPin = gpioCtl.OpenPin(irqPin);
+                _irqPin.SetDriveMode(GpioPinDriveMode.Input);
+                _irqPin.ValueChanged += _irqPin_ValueChanged;
+            }
+
+            var settings = new SpiConnectionSettings()
+            {
+                ChipSelectActiveState = false,
+                ChipSelectLine = csPin,
+                ChipSelectType = SpiChipSelectType.Gpio,
+                ClockFrequency = 2_000_000,
+                DataBitLength = 8,
+                Mode = SpiMode.Mode0
+            };
+
+            _spi = SpiController.FromName(spiBus).GetDevice(settings);
+
+            HardReset();
+            SetDefaultValues();
+        }
+
+        private void SetDefaultValues()
+        {
+            // Force 100% Modulation
+            WriteRegister(Register.TxASK, 0x40);
+
+            // Set CRC to ?
+            WriteRegister(Register.Mode, 0x3D);
+
+            EnableAntennaOn();
+        }
+
+        private void WriteRegister(byte register, byte data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void EnableAntennaOn()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void HardReset()
+        {
+            _resetPin.Write(GpioPinValue.Low);
+            Thread.Sleep(1);
+            _resetPin.Write(GpioPinValue.High);
+            Thread.Sleep(1);
+        }
+
+        private void _irqPin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
