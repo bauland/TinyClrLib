132939b7024ebf7b67bad0f0135323f9a78ee41b
Nicolas Bauland <nicolas.bauland@laposte.net>
2021-04-10 10:27:59 +0200

    Correct Pin version Remove warning message

--- Modules/postbuild.bat
+++ Modules/postbuild.bat
@@ -2,6 +2,6 @@ set ProjectPath=%1
 set TargetDir=%2
 set ConfigurationName=%3
 
-nuget pack "%ProjectPath%" -Properties Configuration=%ConfigurationName%
+nuget pack "%ProjectPath%" -Properties Configuration=%ConfigurationName%;NoWarn=NU5128 
 if defined NUGET_REPOSITORY (echo NUGET_REPOSITORY is defined: %NUGET_REPOSITORY%) else (echo NUGET_REPOSITORY is NOT defined; exit 1)
 move /Y "%TargetDir%*.nupkg" "%NUGET_REPOSITORY%"
